3fef8e4167b562bb728aca2c6998b79a8501df40
Anthony Fox <anthonyfox1988@gmail.com>
2017-11-24 06:09:24 +0000

    Add usage_meter.ini

--- webserver/usage_meter/views.py
+++ webserver/usage_meter/views.py
@@ -8,12 +8,14 @@ from .utils import read_json_file
 @app.route('/', methods=['GET'])
 def index():
     path = app.config.get('USAGE_JSON_FILE_PATH')
-    write_usage_json_to_file(
-        username=app.config.get('COX_USERNAME'),
-        password=app.config.get('COX_PASSWORD'),
-        path=path
-    )
     usage_json = read_json_file(path)
+    if not usage_json:
+        write_usage_json_to_file(
+            username=app.config.get('COX_USERNAME'),
+            password=app.config.get('COX_PASSWORD'),
+            path=path
+        )
+        usage_json = read_json_file(path)
 
     if request.args.get('as_string', False):
         return format_usage_message(usage_json)
